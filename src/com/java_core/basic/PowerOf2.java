2326f4b08086a289bb2ccd5425482a8ee5d26766
Duk <dhugkar@gmail.com>
2021-11-01 17:16:28 +0530

    Added PowerOF2.java

--- src/com/java_core/basic/PowerOf2.java
+++ src/com/java_core/basic/PowerOf2.java
@@ -7,14 +7,24 @@ public class PowerOf2 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number from 0-30");  
 		int pow = sc.nextInt();
-		sc.close();
+		
 		int sum = 1;
+		if(pow>30||pow<0) {
+			while(pow>30||pow<0) {
+				System.out.println("Enter Valid number from 0-30");    
+				pow=sc.nextInt();
+			}
+		}
+		sc.close();
+	
 		for (int i = 0; i < pow; i++) {
 			sum = sum * 2;
 		}
 		System.out.println("2ˆ" + pow + " = " + sum);
-
+		
+		
 	}
 
 }
